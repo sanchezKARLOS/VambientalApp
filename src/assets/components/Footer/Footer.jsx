427883abae6fffab533ab8157ea77cfda2301030
sanchezKARLOS <calbertosanchez@unicesar.edu.co>
2023-10-01 04:01:21 -0500

    Subiendo...

--- src/assets/components/Footer/Footer.jsx
+++ src/assets/components/Footer/Footer.jsx
@@ -5,20 +5,20 @@ function Footer() {
       <footer className="site-footer w-full">
         <div className="container _cwf w-full">
           <div className="row _rowMB w-full flex">
-            <div  id="_cs1" className="col-sm-12 h-full justify-between">
-              <div>
-                <h6 className="font-medium text-gray-700 pl-2 _tcenter _textF">
+            <article id="_cs1" className="col-sm-12">
+              <div className="_pl20">
+                <h6 className="font-medium _textF _text-w">
                   Plataforma Virtual para el Monitoreo de Variables Ambientales
                 </h6>
               </div>
               <div className="content-icon _cim p-2">
-              <p className="_textF">
-              Monitoreamos y mejoramos variables ambientales en tiempo real
-                para un entorno educativo saludable y de calidad. ¡Únete a
-                nuestra comunidad en línea!
-              </p>
+                <p className="_textF _tpm0">
+                  Monitoreamos y mejoramos variables ambientales en tiempo real
+                  para un entorno educativo saludable y de calidad. ¡Únete a
+                  nuestra comunidad en línea!
+                </p>
               </div>
-            </div>
+            </article>
             <div className="_dgmi _dmgiAfter _dgmiG _dgmiP">
               <img
                 className="_cgmi"
@@ -33,15 +33,14 @@ function Footer() {
           <div className="row">
             <div className="col-md-8 col-sm-6 col-xs-12">
               <div className="flex w-full">
-                <div className="w-full bg-white grid">
+                <div className="w-full grid">
                   <div>
-                    {" "}
-                    <p className="text-justify t-c p-4 inline-block _textF">
+                    <p className="text-justify t-c p-4 inline-block _textF _text-w">
                       Universidad Popular del Cesar, Seccional Aguachica.
                     </p>
                   </div>
-                  <div className="m-2">
-                    <a className="get-movil _textF" href="#">
+                  <div className="m-4">
+                    <a className="get-movil _textF _text-w" href="#">
                       Obten la aplicacion Movil
                     </a>
                   </div>
@@ -50,16 +49,18 @@ function Footer() {
             </div>
 
             <div className="col-md-4 col-sm-6 col-xs-12 ">
-              <ul className="social-icons">
-                <li className="_tp">
-                  <p className="copyright-text inline-block w-full _textF">
+              <ul className="social-icons">  
+                  <p className="copyright-text inline-block w-full _textF _text-w">
                     Copyright &copy; 2023 - Ambiente Web
                   </p>
-                </li>
               </ul>
             </div>
+
+
           </div>
         </div>
+
+
       </footer>
     </>
   );
